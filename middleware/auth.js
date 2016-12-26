6feb18ee90f9e58892d0dabafef000e30da1c3f7
Patricio López <lopezjuripatricio@gmail.com>
2016-12-26 19:04:28 -0300

    Refactor

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,63 @@
+const keystone = require('keystone');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookTokenStrategy = require('passport-facebook-token');
+const config = require('../config').auth;
+
+const facebookVerify = (accessToken, refreshToken, profile, done) => {
+  const User = keystone.list('User').model;
+
+  User.findOne({ 'facebook.id': profile.id })
+    // Update or create
+    .then(result => result || new User())
+    .then(user => Object.assign(user, {
+      facebook: {
+        id: profile.id,
+        profile: JSON.stringify(profile, null, 4),
+        accessToken,
+        refreshToken,
+      },
+    }))
+    .then(user => user.save())
+    .then(user => done(null, user))
+    .catch(done);
+};
+
+passport.use(new FacebookTokenStrategy(config.facebook, facebookVerify));
+passport.use(new FacebookStrategy(config.facebook, facebookVerify));
+passport.use(new LocalStrategy((email, password, done) => {
+  const User = keystone.list('User').model;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return done(null, false, { message: 'Email not found.' });
+    }
+    return user.verifyPassword(password).then(valid => {
+      if (!valid) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }).catch(done);
+}));
+
+/* Exports middlewares */
+
+exports.local = passport.authenticate('basic', {
+  session: false,
+});
+
+exports.facebook = passport.authenticate('facebook', {
+  session: false,
+  failureRedirect: '/',
+  // scope: [],
+});
+
+exports.facebookToken = passport.authenticate('facebook-token', {
+  session: false,
+});
+
 exports.authenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,49 +1,6 @@
 const express = require('express');
 const keystone = require('keystone');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const FacebookTokenStrategy = require('passport-facebook-token');
-const config = require('../../config').auth;
-
-
-const facebookVerify = (accessToken, refreshToken, profile, done) => {
-  const User = keystone.list('User').model;
-
-  User.findOne({ 'facebook.id': profile.id })
-    // Update or create
-    .then(result => result || new User())
-    .then(user => Object.assign(user, {
-      facebook: {
-        id: profile.id,
-        profile: JSON.stringify(profile, null, 4),
-        accessToken,
-        refreshToken,
-      },
-    }))
-    .then(user => user.save())
-    .then(user => done(null, user))
-    .catch(done);
-};
-
-passport.use(new FacebookTokenStrategy(config.facebook, facebookVerify));
-passport.use(new FacebookStrategy(config.facebook, facebookVerify));
-passport.use(new LocalStrategy((email, password, done) => {
-  const User = keystone.list('User').model;
-
-  User.findOne({ email }).then(user => {
-    if (!user) {
-      return done(null, false, { message: 'Email not found.' });
-    }
-    return user.verifyPassword(password).then(valid => {
-      if (!valid) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }).catch(done);
-}));
-
+const auth = require('../../middleware/auth');
 
 const router = new express.Router();
 
@@ -53,33 +10,21 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/auth/local', passport.authenticate('basic', {
-  session: false,
-}), (req, res) => {
+router.post('/auth/local', auth.local, (req, res) => {
   res.json({ id: req.user.id, username: req.user.username });
 });
 
-router.get('/auth/facebook', passport.authenticate('facebook', {
-  session: false,
-  // scope: [],
-}));
+router.get('/auth/facebook', auth.facebook);
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  session: false,
-  failureRedirect: '/',
-}), (req, res) => {
-  res.redirect(`/api/profile?access_token=${req.user.facebook.accessToken}`); // ?accessToken=''
+router.get('/auth/facebook/callback', auth.facebook, (req, res) => {
+  res.redirect(`${req.baseUrl}/profile?access_token=${req.user.facebook.accessToken}`);
 });
 
-router.post('/auth/facebook/token', passport.authenticate('facebook-token', {
-  session: false,
-}), (req, res) => {
+router.post('/auth/facebook/token', auth.facebookToken, (req, res) => {
   res.json(req.user);
 });
 
-router.get('/profile', passport.authenticate('facebook-token', {
-  session: false,
-}), (req, res) => {
+router.get('/profile', auth.facebookToken, (req, res) => {
   res.json(req.user);
 });
 
